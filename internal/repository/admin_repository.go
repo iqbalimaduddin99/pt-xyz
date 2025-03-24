0f8b01f4ab6e8dde45fbeee68cc7de6fe2b8ceb9
iqbalimaduddin0 <iqbalimaduddin0@gmail.com>
2025-03-24 13:01:05 +0700

    add feature user

--- internal/repository/admin_repository.go
+++ internal/repository/admin_repository.go
@@ -7,8 +7,9 @@ import (
 )
 
 type RepositoryAdmin interface {
-	CheckAdminExists() (bool, error)
-	InsertAdmin(admin *entities.Admin) error
+	GetAdmin() (bool, error)
+	CreateAdmin(admin *entities.Admin) error
+	GetAdminByUserName(userName string) (*entities.Admin, error)
 }
 
 type RepositoryAdminImpl struct {
@@ -19,7 +20,7 @@ func NewRepositoryAdmin(db *sqlx.DB) *RepositoryAdminImpl  {
 	return &RepositoryAdminImpl{db : db}
 }
 
-func (r *RepositoryAdminImpl) CheckAdminExists() (bool, error) {
+func (r *RepositoryAdminImpl) GetAdmin() (bool, error) {
     query := `SELECT COUNT(*) FROM admin`
     
     var count int
@@ -31,7 +32,7 @@ func (r *RepositoryAdminImpl) CheckAdminExists() (bool, error) {
     return count > 0, nil
 }
 
-func (r *RepositoryAdminImpl) InsertAdmin(admin *entities.Admin) error {
+func (r *RepositoryAdminImpl) CreateAdmin(admin *entities.Admin) error {
     query := `
         INSERT INTO admin (user_name, password, full_name)
         VALUES (:user_name, :password, :full_name)
@@ -39,4 +40,18 @@ func (r *RepositoryAdminImpl) InsertAdmin(admin *entities.Admin) error {
 
     _, err := r.db.NamedExec(query, admin)
     return err
+}
+
+
+
+func (r *RepositoryAdminImpl) GetAdminByUserName(userName string) (*entities.Admin, error) {
+	query := `SELECT * FROM admin WHERE user_name = ?`
+	
+	var admin entities.Admin
+	err := r.db.Get(&admin, query, userName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &admin, nil
 }
